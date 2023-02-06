5b7393a0d30c7e44babd849f452e95b401b2c94f
= <=>
2023-02-06 09:26:20 +0300

    Created project

---
+++ app/src/main/java/com/ghennadiiganenko/android/ghennadiiganenko_quiz_project/fragments/game/GameFragment.kt
@@ -0,0 +1,23 @@
+package com.ghennadiiganenko.android.ghennadiiganenko_quiz_project.fragments.game
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ghennadiiganenko.android.ghennadiiganenko_quiz_project.R
+
+class GameFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game, container, false)
+    }
+}
